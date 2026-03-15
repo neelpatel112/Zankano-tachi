e7332799d639b42e70e41d7b80f85d4d37ed1979
Neel patel <neel48148@gmail.com>
2026-03-15 12:26:32 +0530

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern',
+          additionalData: `@use "@styles/_variables.scss" as *; @use "@styles/_mixins.scss" as *;`
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        '@components': '/src/components',
+        '@styles': '/src/styles',
+        '@data': '/src/data',
+        '@types_': '/src/types'
+      }
+    }
+  }
+});
